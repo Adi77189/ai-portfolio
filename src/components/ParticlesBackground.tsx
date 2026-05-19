47b93587db74a49756ebe7d116930f6146b38a4f
Aditya Singh Bhadauria <adityabhadauria1904@gmail.com>
2026-05-20 05:29:27 +0530

    fixed tsparticles final

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,32 +1,26 @@
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadSlim } from "tsparticles-slim";
-import type { Engine } from "tsparticles-engine";
+import Particles from "@tsparticles/react";
 
 const ParticlesBackground = () => {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadSlim(engine);
-  }, []);
-
   return (
     <Particles
       id="tsparticles"
-      init={particlesInit}
       options={{
         fullScreen: {
           enable: true,
-          zIndex: -10,
+          zIndex: -1,
         },
 
         background: {
-          color: "transparent",
+          color: {
+            value: "transparent",
+          },
         },
 
-        fpsLimit: 120,
+        fpsLimit: 60,
 
         particles: {
           number: {
-            value: 70,
+            value: 60,
           },
 
           color: {
@@ -51,7 +45,10 @@ const ParticlesBackground = () => {
           },
 
           size: {
-            value: { min: 1, max: 3 },
+            value: {
+              min: 1,
+              max: 3,
+            },
           },
         },
 
